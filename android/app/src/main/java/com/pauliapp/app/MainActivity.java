1c0be86010bb5003c4ecd3f57e2444f3066efb2e
Rui Correia <117397560+RuiCorreia15@users.noreply.github.com>
2026-05-03 12:52:19 +0000

    fix: clean standalone Android WebView app

--- android/app/src/main/java/com/pauliapp/app/MainActivity.java
+++ android/app/src/main/java/com/pauliapp/app/MainActivity.java
@@ -1,5 +1,34 @@
 package com.pauliapp.app;
 
-import com.getcapacitor.BridgeActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends Activity {
+  private WebView webView;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    webView = new WebView(this);
+    setContentView(webView);
+    WebSettings settings = webView.getSettings();
+    settings.setJavaScriptEnabled(true);
+    settings.setDomStorageEnabled(true);
+    webView.setWebViewClient(new WebViewClient());
+    webView.loadUrl("https://pauliapp.netlify.app");
+  }
+
+  @Override
+  public void onBackPressed() {
+    if (webView.canGoBack()) {
+      webView.goBack();
+    } else {
+      super.onBackPressed();
+    }
+  }
+}
